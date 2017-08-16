d2a2a53f2f7a18ac65e1994875bf4170a2df0974
William <william.chalons@gmail.com>
2017-08-16 14:44:26 +0200

    Implemented form with devise and starting CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :email_confirmation, :phone, :password, :password_confirmation])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  attr_accessor :email, :email_confirmation
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, confirmation: true
+end
